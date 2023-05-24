3e76bfcfeaff01d95300b0f2018758260ce744a1
Purinat Sanbundit <37452245+PurinatG-01@users.noreply.github.com>
2023-05-24 11:25:54 +0700

    feat: update ticket app 

--- repository/ticket_repository.go
+++ repository/ticket_repository.go
@@ -6,4 +6,6 @@ type TicketRepository interface {
 	All() (*[]model.Ticket, error)
 	Create(event *model.Ticket) (bool, error)
 	Get(id int) (model.Ticket, error)
+	Update(ticket *model.Ticket) (bool, error)
+	Delete(ticket *model.Ticket) (bool, error)
 }
--- service/ticket_service.go
+++ service/ticket_service.go
@@ -3,11 +3,15 @@ package service
 import (
 	model "gin-learning/models"
 	"gin-learning/repository"
+	"time"
 )
 
 type TicketService interface {
 	All() (*[]model.Ticket, error)
 	Create(ticket model.Ticket) (bool, error)
+	Get(id int) (model.Ticket, error)
+	Delete(id int) (bool, error)
+	Update(id int, ticket model.Ticket) (bool, error)
 }
 
 func NewTicketService(repository repository.TicketRepository) TicketService {
@@ -26,4 +30,29 @@ func (s *ticketService) All() (*[]model.Ticket, error) {
 func (s *ticketService) Create(ticket model.Ticket) (bool, error) {
 	_, err := s.repository.Create(&ticket)
 	return true, err
+}
+
+func (s *ticketService) Get(id int) (model.Ticket, error) {
+	ticket, err := s.repository.Get(id)
+	return ticket, err
+}
+
+func (s *ticketService) Delete(id int) (bool, error) {
+	ticket := model.Ticket{Id: id}
+	_, err := s.repository.Delete(&ticket)
+	if err != nil {
+		return true, err
+	}
+	return true, nil
+}
+
+func (s *ticketService) Update(id int, ticket model.Ticket) (bool, error) {
+	ticket.Id = id
+	now := time.Now()
+	ticket.UpdatedAt = &now
+	_, err := s.repository.Update(&ticket)
+	if err != nil {
+		return true, err
+	}
+	return true, nil
 }
