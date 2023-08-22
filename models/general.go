61fbe8ea7d50a7f868cfa24b72cf5ab6ea57b5cc
Purinat Sanbundit <37452245+PurinatG-01@users.noreply.github.com>
2023-08-22 10:30:09 +0700

    fix: fix transaction by userId pagination

--- models/general.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type OrderBy = string
-
-const (
-	ORDER_ASC  OrderBy = "ASC"
-	ORDER_DESC OrderBy = "DESC"
-)
---
+++ models/query.go
@@ -0,0 +1,8 @@
+package model
+
+type OrderBy = string
+
+const (
+	ORDER_BY_ASC  OrderBy = "ASC"
+	ORDER_BY_DESC OrderBy = "DESC"
+)
